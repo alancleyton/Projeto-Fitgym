b13b11884f3489b67ffdea28ecba57527bff328a
Alan Cleyton <alancleyton67@gmail.com>
2020-02-12 15:06:06 -0300

    Adicionado animação ao text-box

--- src/template/widgets/navbar.jsx
+++ src/template/widgets/navbar.jsx
@@ -5,11 +5,11 @@ export default props => (
     <div className="navbar">
         <ul>
             <li><a href="#/">Home</a></li>
-            <li><a href="#/classe">Classes</a></li>
-            <li><a href="#/projetos">Projetos</a></li>
-            <li><a href="#/treinadores">Treinadores</a></li>
+            <li><a href="#/classes">Classes</a></li>
+            <li><a href="#/workouts">Workouts</a></li>
+            <li><a href="#/trainers">Trainers</a></li>
             <li><a href="#/blog">Blog</a></li>
-            <li><a href="#/contato">Contato</a></li>
+            <li><a href="#/contact">Contact</a></li>
         </ul>
     </div>
 );
